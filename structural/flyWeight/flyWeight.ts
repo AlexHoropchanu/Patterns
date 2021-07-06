70d3e3c12e29886ae611f5cdcd7c96ad1106ec3b
unknown <alexhoropchanu@gmail.com>
2021-07-06 16:00:52 +0300

    update

---
+++ structural/flyWeight/flyWeight.ts
@@ -0,0 +1,31 @@
+class Cachee {
+	type: string
+	constructor(type: string = 'image') {
+		this.type = type
+	}
+	setWeight() {
+		console.log(`Создан кэш типа ${this.type}.`)
+	}
+}
+
+class ChairFactory {
+	cache: Array <Cachee> = []
+	getCacheType(type: string = 'image') {
+		for (var i = this.cache.length - 1; i >= 0; i--) {
+			if (this.cache[i].type == type) {
+				console.log('Возврат закешиорованного объекта.')
+				console.log(this.cache[i])
+				return this.cache[i]
+			}
+		}
+		console.log('\nСоздание нового и добавление в кеш объекта.\n')
+		this.cache.push(new Cachee(type))
+	}
+}
+
+let factory = new ChairFactory
+
+factory.getCacheType()
+factory.getCacheType('image')
+factory.getCacheType('login')
+factory.getCacheType('password')
